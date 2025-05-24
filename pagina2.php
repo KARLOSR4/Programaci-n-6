98db1faf7381c639561e8b0e5f107a9ea658cb8c
KARLOSR4 <javier.venturadiaz@unidemat.edu.mx>
2025-05-23 20:50:53 -0500

    Add files via upload
    Practica 3

---
+++ pagina2.php
@@ -0,0 +1,17 @@
+<?php
+$imgn=$_GET['imgn'];
+echo "<img src='$imgn'>";
+?>
+<style>
+        body {
+            background-color: rgb(218, 216, 214);
+            background-image: url('imagenes/fondo 2.jpg');
+            background-size: cover;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
